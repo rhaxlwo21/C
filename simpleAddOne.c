bea3012eb6529b4d8862afc2d7fc4cd52fabbf11
SeoJeongHyun <30520300+rhaxlwo21@users.noreply.github.com>
2018-07-24 23:35:13 +0900

    Create simpleAddOne.c

---
+++ simpleAddOne.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2;	//변수 num1,num2
+	int num3 = 30, num4 = 40;	//변수 num3,num4의 선언 및 초기화
+
+	printf("num1: %d, num2: %d \n", num1, num2);
+	printf("num3: %d, num4: %d \n", num3, num4);
+	return 0;
+}
+
+
+//num1, num2는 쓰레기 값에 의해 오류가 나타남
